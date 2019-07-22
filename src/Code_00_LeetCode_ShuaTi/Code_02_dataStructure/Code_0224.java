fcfadf24e7e71eaff8b47396767b9ff4e0106fe0
jchen <1443616037@qq.com>
2019-07-23 00:48:40 +0800

    daily commit

---
+++ src/Code_00_LeetCode_ShuaTi/Code_02_dataStructure/Code_0224.java
@@ -0,0 +1,4 @@
+package Code_00_LeetCode_ShuaTi.Code_02_dataStructure;
+
+public class Code_0224 {
+}
---
+++ src/Code_00_LeetCode_ShuaTi/Code_02_dataStructure/Code_0225.java
@@ -0,0 +1,44 @@
+package Code_00_LeetCode_ShuaTi.Code_02_dataStructure;
+
+//https://leetcode-cn.com/problems/implement-stack-using-queues/
+
+public class Code_225 {
+}
+
+
+class MyStack {
+
+    /** Initialize your data structure here. */
+    public MyStack() {
+
+    }
+
+    /** Push element x onto stack. */
+    public void push(int x) {
+
+    }
+
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        return 0;
+    }
+
+    /** Get the top element. */
+    public int top() {
+        return 0;
+    }
+
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return false;
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
