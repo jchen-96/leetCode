6c6e3977a98bb1dd81f8ee983c263d0b206ac876
jchen <1443616037@qq.com>
2019-07-24 22:37:26 +0800

    daily commit

--- src/Code_00_LeetCode_ShuaTi/Code_02_dataStructure/Code_0295.java
+++ src/Code_00_LeetCode_ShuaTi/Code_02_dataStructure/Code_0295.java
@@ -93,13 +93,5 @@ class MedianFinder {
 
     }
 
-//    public static void main(String[] args) {
-//        MedianFinder medianFinder=new MedianFinder();
-//        medianFinder.addNum(1);
-//        medianFinder.addNum(2);
-//        System.out.println(medianFinder.findMedian());
-//        medianFinder.addNum(3);
-//        System.out.println(medianFinder.findMedian());
-//
-//    }
+
 }
